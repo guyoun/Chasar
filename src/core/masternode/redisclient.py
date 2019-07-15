2b30e10b4ffa8c0d17d89b9e1cff2d16598f1db3
Gu Youn <urbane9@gmail.com>
2019-07-15 16:57:28 +0900

    Fix a command in redisclient is wrong

--- src/core/masternode/redisclient.py
+++ src/core/masternode/redisclient.py
@@ -9,4 +9,4 @@ class RedisClient:
 
     def save(self, data_json, client_id):
         channel_name = ("Chasar-Client:%s" % client_id)
-        self.redis.ZADD([channel_name, data_json])
+        self.redis.rpush(channel_name, data_json)
